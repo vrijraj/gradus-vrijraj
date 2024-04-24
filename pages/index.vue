9a0c2fbc4bce996d9aaffedbe575abc07425f8f9
vrijraj <vrijraj2396@gmail.com>
2024-04-24 06:39:25 +0530

    home ui updated

--- pages/index.vue
+++ pages/index.vue
@@ -7,82 +7,150 @@
 
       <v-app-bar-title>Application Bar</v-app-bar-title>
     </v-app-bar>
-    <v-container>
-      <v-row>
-        <v-col>
-          <v-toolbar>
-            <v-text-field
-              v-model="search.value"
-              hide-details="auto"
-              variant="outlined"
-              dense
-              label="Search"
-            ></v-text-field>
+    <v-container fluid style="background-color: #e8f0fe">
+      <v-row justify="center" align="center">
+        <v-col md="11">
+          <v-container fluid>
+            <v-row justify="center" align="center">
+              <v-col md="8">
+                <h1 style="font-size: xx-large">Welcome to CodeLabs</h1>
+                <p class="mb-3">
+                  This is a collection of well curated articles
+                </p>
 
-            <v-spacer></v-spacer>
-            <v-select
-              label="Select"
-              variant="outlined"
-              multiple
-              dense
-              :items="['firebase', 'nodejs']"
-            ></v-select>
-          </v-toolbar>
+                <v-btn variant="flat" size="small" class="mr-2 mb-2" icon
+                  ><v-icon size="large">mdi-linkedin</v-icon></v-btn
+                >
+                <v-btn variant="flat" size="small" class="mr-2 mb-2" icon
+                  ><v-icon size="large">mdi-twitter</v-icon></v-btn
+                >
+                <v-btn variant="flat" size="small" class="mr-2 mb-2" icon
+                  ><v-icon size="large">mdi-github</v-icon></v-btn
+                >
+                <v-btn variant="flat" size="small" class="mr-2 mb-2" icon
+                  ><v-icon size="large">mdi-web</v-icon></v-btn
+                >
+                <v-btn variant="flat" size="small" class="mr-2 mb-2" icon
+                  ><v-icon size="large">mdi-youtube</v-icon></v-btn
+                >
+              </v-col>
+              <v-col md="4" class="text-center">
+                <v-img
+                  width="70%"
+                  :src="'/donotremove/gradus-background.svg'"
+                ></v-img>
+              </v-col>
+            </v-row>
+          </v-container>
         </v-col>
       </v-row>
-      <v-row>
-        <!-- Search -->
-        <div v-if="Object.keys(res).length > 0">
+    </v-container>
+    <v-container fluid>
+      <!-- Search Header -->
+      <v-row
+        justify="center"
+        align="center"
+        style="border-bottom: 1px solid #e8f0fe"
+      >
+        <v-col md="11">
           <v-row>
-            <v-col md="4" v-for="article in res.value" :key="index">
-              <v-card>
-                <v-img :src="article.image"></v-img>
-                <v-card-title>{{ article.title }}</v-card-title>
-                <v-card-text>
-                  <p>{{ article.description }}</p>
-                  <v-chip v-for="(tag, index) in article.tags" :key="index">{{
-                    tag
-                  }}</v-chip>
-                  <br />
-                  <br />
-                  <NuxtLink :to="article._path"
-                    ><v-btn variant="tonal">asd</v-btn></NuxtLink
-                  >
-                </v-card-text>
-                <!-- {{ article }} -->
-
-                <!-- <v-btn :to="article._path"></v-btn> -->
-              </v-card>
+            <v-col md="2">
+              <v-text-field
+                v-model="search.value"
+                hide-details="auto"
+                rounded
+                variant="outlined"
+                clearable
+                bg-color="#E8F0FE"
+                append-inner-icon="mdi-magnify"
+                density="compact"
+                label="Search for any topic"
+              ></v-text-field>
+            </v-col>
+            <v-col md="2">
+              <v-select
+                label="Choose Topic"
+                density="compact"
+                variant="outlined"
+                multiple
+                rounded
+                dense
+                bg-color="#E8F0FE"
+                prepand-inner-icon="mdi-magnify"
+                :items="['firebase', 'nodejs']"
+              ></v-select>
             </v-col>
           </v-row>
-          <!-- <NuxtLink v-for="r in res.value" :to="r.id">{{ r.title }}</NuxtLink> -->
-        </div>
-
-        <v-row>
-          <ContentList path="/" v-slot="{ list }" :query="{ draft: false }">
-            <v-col md="4" v-for="article in list" :key="article._path">
-              <v-card>
-                <v-img :src="article.image"></v-img>
-                <v-card-title>{{ article.title }}</v-card-title>
-                <v-card-text>
-                  <p>{{ article.description }}</p>
-                  <v-chip v-for="(tag, index) in article.tags" :key="index">{{
-                    tag
-                  }}</v-chip>
-                  <br />
-                  <br />
-                  <NuxtLink :to="article._path"
-                    ><v-btn variant="tonal">asd</v-btn></NuxtLink
-                  >
-                </v-card-text>
-                <!-- {{ article }} -->
-
-                <!-- <v-btn :to="article._path"></v-btn> -->
-              </v-card>
-            </v-col>
-          </ContentList>
-        </v-row>
+        </v-col>
       </v-row>
+      <!-- Search Header -->
+
+      <!-- Search -->
+      <v-row justify="center" align="center">
+        <v-col md="11">
+          <v-container fluid>
+            <div v-if="Object.keys(res).length > 0">
+              <v-row>
+                <v-col md="4" v-for="article in res.value" :key="index">
+                  <v-card>
+                    <v-img :src="article.image"></v-img>
+                    <v-card-title>{{ article.title }}</v-card-title>
+                    <v-card-text>
+                      <p>{{ article.description }}</p>
+                      <v-chip
+                        v-for="(tag, index) in article.tags"
+                        :key="index"
+                        >{{ tag }}</v-chip
+                      >
+                      <br />
+                      <br />
+                      <NuxtLink :to="article._path"
+                        ><v-btn variant="tonal">asd</v-btn></NuxtLink
+                      >
+                    </v-card-text>
+                    <!-- {{ article }} -->
+
+                    <!-- <v-btn :to="article._path"></v-btn> -->
+                  </v-card>
+                </v-col>
+              </v-row>
+              <!-- <NuxtLink v-for="r in res.value" :to="r.id">{{ r.title }}</NuxtLink> -->
+            </div>
+          </v-container>
+        </v-col>
+      </v-row>
+      <!-- Search -->
+
+      <!-- All Data -->
+      <v-row justify="center" align="center" class="py-0 my-0">
+        <v-col md="11" class="py-0 my-0">
+          <v-row class="py-0 my-0">
+            <ContentList path="/" v-slot="{ list }" :query="{ draft: false }">
+              <v-col md="2" v-for="article in list" :key="article._path">
+                <v-card>
+                  <v-img :src="article.image"></v-img>
+                  <v-card-title>{{ article.title }}</v-card-title>
+                  <v-card-text>
+                    <p>{{ article.description }}</p>
+                    <v-chip v-for="(tag, index) in article.tags" :key="index">{{
+                      tag
+                    }}</v-chip>
+                    <br />
+                    <br />
+                    <NuxtLink :to="article._path"
+                      ><v-btn variant="tonal">asd</v-btn></NuxtLink
+                    >
+                  </v-card-text>
+                  <!-- {{ article }} -->
+
+                  <!-- <v-btn :to="article._path"></v-btn> -->
+                </v-card>
+              </v-col>
+            </ContentList>
+          </v-row>
+        </v-col>
+      </v-row>
+      <!-- All Data -->
     </v-container>
   </v-main>
 </template>
