7075e8d19bf7bb11659c00a3798484c0f5d58891
vrijraj <vrijraj2396@gmail.com>
2024-04-27 23:02:28 +0400

    ui fixed

--- components/content/ProseBlockquote.vue
+++ components/content/ProseBlockquote.vue
@@ -1,6 +1,6 @@
 <template>
-   <v-card style="background-color: #E6F4EA;border: 1px solid #137333;" elevation="0" class="pa-3 my-3">
-    <p style="line-height: normal;font-size: 90%;padding: 0 !important;"><slot/></p>
+   <v-card style="background-color: #E6F4EA;border-left: 7px solid #137333;;" elevation="0" class="pa-3 my-3">
+    <p style="line-height: normal;font-size: 100%;padding: 0 !important;"><slot/></p>
   </v-card>
 </template>
 
