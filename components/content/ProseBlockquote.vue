0980c48baee2faa713358417944f98b7fdf504aa
vrijraj <vrijraj2396@gmail.com>
2024-04-27 22:59:09 +0400

    ui fixed

---
+++ components/content/ProseBlockquote.vue
@@ -0,0 +1,15 @@
+<template>
+   <v-card style="background-color: #E6F4EA;border: 1px solid #137333;" elevation="0" class="pa-3 my-3">
+    <p style="line-height: normal;font-size: 90%;padding: 0 !important;"><slot/></p>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
