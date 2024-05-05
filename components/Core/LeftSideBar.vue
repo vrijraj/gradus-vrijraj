f6dc09e9381c3bd4840b0ac9a7e201f204557809
vrijraj <vrijraj2396@gmail.com>
2024-05-05 18:56:57 +0530

    ui fixed

--- components/Core/LeftSideBar.vue
+++ components/Core/LeftSideBar.vue
@@ -1,37 +1,44 @@
 <template>
   <v-navigation-drawer
     floating
+    app
     elevation="0"
     width="300"
-	v-if="stepDrawer"
+    v-if="stepDrawer"
     style="background-color: #f5f8fc"
   >
-
-	<slot></slot>
+    <slot></slot>
     <!-- Author -->
-    <CoreAuthorCard :authors="authors" />
-    <v-sheet class="d-flex mx-0 my-3" style="background-color: #f5f8fc">
-      <v-sheet
-        class="ma-0 pa-0 me-auto align-self-center"
-        style="background-color: #f5f8fc"
-      >
-        <a href="http://" target="_blank">
-          <NuxtImg width="130" :src="'/donotremove/build-with-gradus.svg'" />
-        </a>
-      </v-sheet>
-      <v-sheet
-        class="ma-0 pa-0 align-self-center"
-        style="background-color: #f5f8fc"
-      >
-        <a
-          href="mailto:vrijraj2396@gmail.com"
-          class="mx-0"
-          style="font-size: 80%"
-        >
-          <v-icon>mdi-bug-outline</v-icon> Report a Bug
-        </a>
-      </v-sheet>
-    </v-sheet>
+    <template v-slot:append>
+      <div class="px-4">
+        <CoreAuthorCard :authors="authors" />
+        <v-sheet class="d-flex mx-0 my-3" style="background-color: #f5f8fc">
+          <v-sheet
+            class="ma-0 pa-0 me-auto align-self-center"
+            style="background-color: #f5f8fc"
+          >
+            <a href="http://" target="_blank">
+              <NuxtImg
+                width="130"
+                :src="'/donotremove/build-with-gradus.svg'"
+              />
+            </a>
+          </v-sheet>
+          <v-sheet
+            class="ma-0 pa-0 align-self-center"
+            style="background-color: #f5f8fc"
+          >
+            <a
+              href="mailto:vrijraj2396@gmail.com"
+              class="mx-0"
+              style="font-size: 80%"
+            >
+              <v-icon>mdi-bug-outline</v-icon> Report a Bug
+            </a>
+          </v-sheet>
+        </v-sheet>
+      </div>
+    </template>
   </v-navigation-drawer>
 </template>
 
